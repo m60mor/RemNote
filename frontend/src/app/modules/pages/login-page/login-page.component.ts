1d57c9b41e59a494f4e272baccfe72b6cdd0dca2
unknown <m60mor@gmail.com>
2024-06-07 01:48:43 +0200

    Improving login page

--- frontend/src/app/modules/pages/login-page/login-page.component.ts
+++ frontend/src/app/modules/pages/login-page/login-page.component.ts
@@ -1,44 +1,65 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     standalone: true,
     templateUrl: './login-page.component.html',
     styleUrl: './login-page.component.scss',
-    imports: [CommonModule, FormsModule]
+    imports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class LoginPage {
-    constructor(private router: Router) {
-        console.log('Login page');
+    loginForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    })
+
+    constructor(private router: Router, private fb: FormBuilder) {
         var stored_jwt = localStorage.getItem('jwt')
-        console.log(stored_jwt);
         if (stored_jwt != null) {
             this.router.navigate(['/notes']);
         }
     }
 
+    isSubmitted = false;
     jwt : any = ''
-    email : any = ''
-    username : any = ''
-    password : any = ''
+    emailValidator() {
+        return this.loginForm.get('email')?.invalid && this.isSubmitted;
+    }
+    usernameValidator() {
+        return this.loginForm.get('username')?.invalid && this.isSubmitted;
+    }
+    passwordValidator() {
+        return this.loginForm.get('password')?.invalid && this.isSubmitted;
+    }
 
+    //(this.loginForm.get('email')?.dirty || this.loginForm.get('email')?.touched || this.isSubmitted)
 
     async login() {
-        const response = await fetch('http://127.0.0.1:5000/login', {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email: this.email ,username: this.username, password: this.password})
-        }).then(response => response.json())
-        .then(data => {
-            this.jwt = data?.[0]?.[0];
-            if (this.jwt != '') {
-                localStorage.setItem('jwt', this.jwt);
-                this.router.navigate(['/notes']);
-            }
-        });
+        this.isSubmitted = true;
+        if (this.loginForm.invalid) {
+            console.log('Invalid form');
+        }
+        else {
+            const response = await fetch('http://127.0.0.1:5000/login', {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email: this.loginForm.value.email ,username: this.loginForm.value.username, password: this.loginForm.value.password})
+            }).then(response => response.json())
+            .then(data => {
+                if (data?.[0]?.[0]) {
+                    this.jwt = data?.[0]?.[0];
+                    console.log('JWT:', this.jwt);
+                    localStorage.setItem('jwt', this.jwt);
+                    this.router.navigate(['/notes']);
+                }})
+            .catch((error) => { 
+                console.error('Error:', error);
+            });
+        }
     }
 }
